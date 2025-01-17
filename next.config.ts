559f9114b436a85198c11a6414ba309abaa44be0
YogeshNayak04 <sisodiyaadhiraj8@gmail.com>
2025-01-17 14:04:16 +0530

    fix1

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   
-};
-
-module.exports = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, 
   },
 };
 
